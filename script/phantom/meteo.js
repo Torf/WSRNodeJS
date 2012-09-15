08fc9ea568aab21f99fa04e750495341a0004d2d
Jean-Philippe Encausse <Jp@encausse.net>
2012-09-15 13:47:50 +0200

    Improve PhantomJS API

--- script/phantom/meteo.js
+++ script/phantom/meteo.js
@@ -1,76 +1,18 @@
 
-var page    = require('webpage').create();
-var system  = require('system');
-var url     = 'http://mobile.meteofrance.com/france/ville/versailles/';
+
+// Inject helper
+phantom.injectJs("../lib/scraper.js");
+
+// Merge default options
 var options = {'zip' :'78000'};
+scraper.setOptions(options);
 
-
-// ------------------------------------------
-//  ARGUMENTS
-// ------------------------------------------
-
-if (system.args.length > 1){
-
-  // Parse JSON
-  var json = system.args[1];
-  var params = json ? JSON.parse(json) : {};
+// Scrap
+var url = 'http://mobile.meteofrance.com/france/ville/versailles/'+options.zip;
+scraper.scrap(url, options, function(options, results){
   
-  // Merge with defaults
-  for (var property in params){
-    options[property] = params[property];
-  }
-}
-
-// ------------------------------------------
-//  EVALUATE
-// ------------------------------------------
-
-var evaluate = function evaluate(page, func) {
-    var args = [].slice.call(arguments, 2);
-    var fn = "function() { return (" + func.toString() + ").apply(this, " + JSON.stringify(args) + ");}";
-    return page.evaluate(fn);
-}
-
-// ------------------------------------------
-//  PAGE.OPEN
-// ------------------------------------------
-
-page.open(encodeURI(url+options.zip), function (status) { 
-  
-  var results = {
-    tts : "Impossible d'accèder au site"
-  }
-  
-  // Check for page load success
-  if (status !== "success") {
-    console.log(JSON.stringify(results));
-    phantom.exit();
-    return;
-  }
-  
-  // Load jQuery
-  if (!page.injectJs("../vendor/jquery.min.js")){
-    console.log(JSON.stringify(results));
-    phantom.exit();
-    return;
-  }
-  
-  // Scrapping
-  var pos = parseInt(options.movie);
-  results = evaluate(page, function(pos) {
-    results = {};
-    
-    var tr = $('DIV#prevision TABLE.prevSem1 TR:nth-child(4)');
-    
-    results.tts  = tr.find('TD:nth-child(1)').text() + ', '; // Days 
-    results.tts += tr.find('TD:nth-child(2) IMG').attr('alt') + ', '; // Sun 
-    results.tts += tr.find('TD:nth-child(3)').text(); // Temperature
-    
-    return results; 
-  }, pos);
-  
-  
-  // Write answer back
-  console.log(JSON.stringify(results));
-  phantom.exit();
+  var tr = $('DIV#prevision TABLE.prevSem1 TR:nth-child(4)');
+  results.tts  = tr.find('TD:nth-child(1)').text() + ', ';          // Days 
+  results.tts += tr.find('TD:nth-child(2) IMG').attr('alt') + ', '; // Sun 
+  results.tts += tr.find('TD:nth-child(3)').text();                 // Temperature
 });
--- script/phantom/movie.js
+++ script/phantom/movie.js
@@ -1,41 +1,23 @@
 
-var page    = require('webpage').create();
-var system  = require('system');
-var url     = 'http://iphone.allocine.fr/salle/seances_gen_csalle=';
-var options = {
- 'place' : 'B0099',
- 'movie' : false
-};
-
-
-// ------------------------------------------
-//  ARGUMENTS
-// ------------------------------------------
-
-if (system.args.length > 1){
-
-  // Parse JSON
-  var json = system.args[1];
-  var params = json ? JSON.parse(json) : {};
-  
-  // Merge with defaults
-  for (var property in params){
-    options[property] = params[property];
-  }
-}
+// Inject helper
+phantom.injectJs("../lib/scraper.js");
 
 // ------------------------------------------
 //  GRAMMAR
 // ------------------------------------------
 
 var setGrammar = function(options, results){
-  var movies = results.movies; results.movies = undefined;
+
+  var movies = results.movies; 
+  if (!movies){ return; }
+  
+  results.movies = undefined;
   if (!options.directory){
     return;
   }
   
-  var fs = require('fs'); // console.log(fs.workingDirectory);
-  var directory = options.directory + '\\movie.xml'; // 'E:\\Dropbox\\Projects\\WSRMacro\\WSRMacro\\macros\\movie.xml';
+  var fs = require('fs');
+  var directory = options.directory + '\\movie.xml';
   var xml = fs.read(directory);
   
   var replace  = '§ -->\n';
@@ -58,86 +40,53 @@ var setGrammar = function(options, results){
 }
 
 // ------------------------------------------
-//  EVALUATE
+//  OPTIONS
 // ------------------------------------------
 
-var evaluate = function evaluate(page, func) {
-    var args = [].slice.call(arguments, 2);
-    var fn = "function() { return (" + func.toString() + ").apply(this, " + JSON.stringify(args) + ");}";
-    return page.evaluate(fn);
-}
+// Merge default options
+var options = {
+ 'place' : 'B0099',
+ 'movie' : false
+};
+scraper.setOptions(options);
+
 
 // ------------------------------------------
-//  PAGE.OPEN
+//  SCRAPING
 // ------------------------------------------
 
-page.open(encodeURI(url + options.place + '.html'), function (status) { 
+
+// Scrap
+var url = 'http://iphone.allocine.fr/salle/seances_gen_csalle='+options.place+'.html';
+scraper.scrap(url, options, function(options, results){
   
-  var results = {
-    tts : "Impossible d'accèder au site"
-  }
-  
-  // Check for page load success
-  if (status !== "success") {
-    console.log(JSON.stringify(results));
-    phantom.exit();
-    return;
-  }
-  
-  // Load jQuery
-  if (!page.injectJs("../vendor/jquery.min.js")){
-    console.log(JSON.stringify(results));
-    phantom.exit();
-    return;
-  }
-  
-  // Scrapping Hours
+  // 2. Parsing Hours of the Movie
   if (options.movie){
-  
-    var pos = parseInt(options.movie);
-    results = evaluate(page, function(pos) {
-      results = {};
-      
-      var theatre = $('DIV.titre B').text();
-      var movie   = $('DIV.cell A[href^="/film"]').get(pos);
-      
-      if (movie){
-        var hours = $(movie).closest('B').siblings('DIV').html();
-            hours = hours.replace(/<br>/g,'. ').replace(/Lun[-,]* /g,'Lundi ')
-                         .replace(/Mar[-,]* /g,'Mardi ').replace(/Mer[-,]* /g,'Mercredi ')
-                         .replace(/Jeu[-,]* /g,'Jeudi ').replace(/Ven[-,]* /g,'Vendredi ')
-                         .replace(/Sam[-,]* /g,'Samedi ').replace(/Dim[-,]* /g,'Dimanche ');
-        
-        var movie = $(movie).text();
-        results.tts = 'Voici les horaires pour '+movie+' au '+theatre+' : '+hours;
-      } else {
-        results.tts = "Je n'ai pas trouvé le film au " + theatre;
-      }
-      
-      return results; 
-    }, pos);
-  
-  // Scrapping Movies  
-  } else {
-    results = page.evaluate(function() {
-      
-      results = {};
-      results.movies = $('DIV.cell A[href^="/film"]').map(function(){ return $(this).text(); }).get();
+    var pos     = parseInt(options.movie);
+    var theatre = $('DIV.titre B').text();
+    var movie   = $('DIV.cell A[href^="/film"]').get(pos);
     
-      results.tts   = 'Voici la liste des films au ';
-      results.tts += $('DIV.titre B').text() + ': ';
-      results.tts += results.movies.join(', ');
+    if (movie){
+      var hours = $(movie).closest('B').siblings('DIV').html();
+          hours = hours.replace(/<br>/g,'. ').replace(/Lun[-,]* /g,'Lundi ')
+                       .replace(/Mar[-,]* /g,'Mardi ').replace(/Mer[-,]* /g,'Mercredi ')
+                       .replace(/Jeu[-,]* /g,'Jeudi ').replace(/Ven[-,]* /g,'Vendredi ')
+                       .replace(/Sam[-,]* /g,'Samedi ').replace(/Dim[-,]* /g,'Dimanche ');
         
-      return results; 
-    });
-    
-    // Generate grammar
-    setGrammar(options, results);
+      var movie = $(movie).text();
+      results.tts = 'Voici les horaires pour '+movie+' au '+theatre+' : '+hours;
+    } else {
+      results.tts = "Je n'ai pas trouvé le film au " + theatre;
+    }
   }
   
-  // Write answer back
-  console.log(JSON.stringify(results));
-  phantom.exit();
-});
-
+  // 1. Parsing List of Movies
+  else {
+    results.movies = $('DIV.cell A[href^="/film"]').map(function(){ return $(this).text(); }).get();
+    
+    results.tts   = 'Voici la liste des films au ';
+    results.tts += $('DIV.titre B').text() + ': ';
+    results.tts += results.movies.join(', ');
+  }
+}, setGrammar);
 
